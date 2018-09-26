fe009f45f83182a599a91245db9bc02bbc6a0ea6
richard <richardsonsantiago@ufrn.edu.br>
2018-09-26 15:48:46 -0300

    up_down_counter

--- cd/up_down_counter/up_down_counter.vhd
+++ cd/up_down_counter/up_down_counter.vhd
@@ -3,7 +3,8 @@ use IEEE.std_logic_1164.all;
 
 entity up_down_counter is
     port (
-        s, en, rst, clk : in std_logic
+        s, en, rst, clk : in std_logic;
+        saida_final : out std_logic_vector(6 downto 0)
     );
 end up_down_counter;
 
@@ -24,13 +25,11 @@ architecture ckt of up_down_counter is
     end component;
 
     signal counter_out : std_logic_vector(2 downto 0);
-    signal segments    : std_logic_vector(6 downto 0);
 
 begin
 
     COUNT : counter port map(s, en, rst, clk, counter_out);
-    DISP  : display_7seg port map(counter_out, segments);
-
+    DISP  : display_7seg port map(counter_out, saida_final);
 end ckt;
 
 
