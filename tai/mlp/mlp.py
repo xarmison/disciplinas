b658852a92a4bd73609d10b964ce537c7f1bb591
richard <richardsonsantiago@ufrn.edu.br>
2018-11-25 15:57:54 -0300

    mlp

--- tai/mlp/mlp.py
+++ tai/mlp/mlp.py
@@ -4,37 +4,39 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 
+
 class NeuralNetwork(object):
     """Fully-connected Multi-Layer Perceptron (MLP)"""
-    def __init__(self, inputSize, hiddenSize, outputSize, learning_rate = 0.01, bias = 1, epochs = 100, debug = False):
+
+    def __init__(self, inputSize, hiddenSize, outputSize, learning_rate=0.01, bias=1, epochs=100, debug=False):
         """Metodo construtor. Define as caracteristicas da MLP"""
-        self.inputSize  = inputSize
+        self.inputSize = inputSize
         self.hiddenSize = hiddenSize
         self.outputSize = outputSize
-        self.lr         = learning_rate
-        self.bias       = bias
-        self.epochs     = epochs
-        self.debug      = debug
+        self.lr = learning_rate
+        self.bias = bias
+        self.epochs = epochs
+        self.debug = debug
 
-        #weights
+        # weights
         np.random.seed(1)
-        self.w1 = np.random.random((self.inputSize + 1, self.hiddenSize)) 
-        self.w2 = np.random.random((self.hiddenSize, self.outputSize)) 
-               
+        self.w1 = np.random.random((self.inputSize + 1, self.hiddenSize))
+        self.w2 = np.random.random((self.hiddenSize, self.outputSize))
+
     def sigmoid(self, x):
         """Implementa a Sigmoid como função de ativação"""
         # Função de ativação
-        return 1/(1 + np.exp(-x) )
+        return 1/(1 + np.exp(-x))
 
     def sigmoidPrime(self, x):
         """Implementa a derivada da Sigmoid"""
-        # Derivada da sigmoid 
-        #return np.exp(-x)/(1 + np.exp(-x))**2
+        # Derivada da sigmoid
+        # return np.exp(-x)/(1 + np.exp(-x))**2
         return x * (1 - x)
 
     def predict(self, inp):
-        net1 = self.sigmoid( np.dot(inp, self.w1) )
-        net2 = self.sigmoid( np.dot(net1,self.w2) )
+        net1 = self.sigmoid(np.dot(inp, self.w1))
+        net2 = self.sigmoid(np.dot(net1, self.w2))
 
         return net2
 
@@ -43,16 +45,16 @@ class NeuralNetwork(object):
         inputs = np.hstack((inputs, np.ones((inputs.shape[0], self.bias))))
 
         start = timer()
-        for i in range(1, self.epochs + 1): 
+        for i in range(1, self.epochs + 1):
 
             l0 = inputs
             l1 = self.sigmoid(np.dot(l0, self.w1))
             l2 = self.sigmoid(np.dot(l1, self.w2))
 
-            l2_err   = outputs - l2
+            l2_err = outputs - l2
             l2_delta = self.lr * np.multiply(l2_err, self.sigmoidPrime(l2))
 
-            l1_err   = np.dot(l2_delta, self.w2.T)
+            l1_err = np.dot(l2_delta, self.w2.T)
             l1_delta = self.lr * np.multiply(l1_err, self.sigmoidPrime(l1))
 
             self.w2 += np.dot(l1.T, l2_delta)
@@ -60,10 +62,10 @@ class NeuralNetwork(object):
 
             if self.debug:
                 print("\nEpoch: {}".format(i))
-                print("Error: {}".format(np.mean(np.square(l2_err))))      
+                print("Error: {}".format(np.mean(np.square(l2_err))))
         end = timer()
         print("Traning took {:.2f}s".format(end-start))
-        
+
     def getWeights(self):
         """Retorna os vetores de peso"""
         print("\n___Weights___")
@@ -78,31 +80,33 @@ class NeuralNetwork(object):
             print("  {:15}".format(np.array_str(i)))
         print("]\n")
 
+
 def importXorDataset():
     # Importa o dataset XOR
-    dataset = pd.read_csv("../data/xor.csv", header=None)
+    dataset = pd.read_csv("/home/richard/repos/disciplinas/tai/data/xor.csv", header=None)
 
     # Extrai todos os rotulos
     labels = dataset.iloc[:, 2].values.astype(float).reshape(2000, 1)
 
     # Extrai todos as entradas
     inputs = dataset.iloc[:, [0, 1]].values.astype(float)
-   
+
     # Divide os dados para treinamento e teste
     train_data, test_data, train_labels, test_labels = train_test_split(
-        inputs, labels, test_size = 0.1, random_state = None
+        inputs, labels, test_size=0.1, random_state=None
     )
 
     return train_data, test_data, train_labels, test_labels
+
 
 def importIrisDataset():
     # Importa o iris dataset
     dataset = pd.read_csv('../data/iris_data.csv', header=None)
 
-    # Extrai os 100 primeiras rotulos 
+    # Extrai os 100 primeiras rotulos
     labels = dataset.iloc[:, 4].values.reshape(150, 1)
-    
-    # Substitui o nome dos rotulos 
+
+    # Substitui o nome dos rotulos
     for i, label in enumerate(labels):
         if(label == 'Iris-setosa'):
             labels[i][0] = 0
@@ -118,11 +122,12 @@ def importIrisDataset():
 
     # Divide os dados para treinamento e teste
     train_data, test_data, train_labels, test_labels = train_test_split(
-        inputs, labels, test_size = 0.1, random_state = None
+        inputs, labels, test_size=0.1, random_state=None
     )
 
     return train_data, test_data, train_labels, test_labels
-   
+
+
 def plotData(x, y, x1):
     import matplotlib.pyplot as plt
 
@@ -133,31 +138,36 @@ def plotData(x, y, x1):
     plt.tight_layout()
     plt.show()
 
+
 if __name__ == "__main__":
 
     print("_____ Multi-Layer Perceptron _____")
-    
+
     print("Importing data ...")
     train_data, test_data, train_labels, test_labels = importXorDataset()
 
     print("Building the network ...")
-    model = NeuralNetwork(2, 5, 1, learning_rate=0.01, epochs=10000, debug=False)
+    model = NeuralNetwork(2, 5, 1, learning_rate=0.01,
+                          epochs=10000, debug=False)
 
     print("Traning ...")
     model.train(train_data, train_labels)
 
-    if(model.debug) :
+    if(model.debug):
         model.getWeights()
-        
+
         print("MLP preditions:")
         for i, item in enumerate(test_data):
             print("Prediction for {:15} is {:3}, expected {}".format(
-                    np.array_str(item), np.array_str(np.round(model.predict(np.append(item, 1)))), test_labels[i] 
-                )
+                np.array_str(item), np.array_str(
+                    np.round(model.predict(np.append(item, 1)))), test_labels[i]
             )
-    
-    pred = np.round(model.predict(np.hstack((test_data, np.ones((test_data.shape[0], 1))))))
+            )
+
+    pred = np.round(model.predict(
+        np.hstack((test_data, np.ones((test_data.shape[0], 1))))))
     print("\nReport: \n{}".format(classification_report(test_labels, pred)))
-    print("Confusion Matrix: \n{}\n".format(confusion_matrix(test_labels, pred)))
-    
-    plotData(train_data, train_labels, test_data)    
+    print("Confusion Matrix: \n{}\n".format(
+        confusion_matrix(test_labels, pred)))
+
+    plotData(train_data, train_labels, test_data)
